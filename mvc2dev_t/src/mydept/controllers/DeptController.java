022e754b763ae9337603858896acf2fc6f309bf8
Admin <Admin@DESKTOP-0J89H1P>
2020-06-22 16:22:59 +0900

    mvc2추가

---
+++ mvc2dev_t/src/mydept/controllers/DeptController.java
@@ -0,0 +1,29 @@
+package mydept.controllers;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import mydept.models.DeptDAO;
+import mydept.models.DeptDTO;
+
+@WebServlet("/dept")
+public class DeptController extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		DeptDAO deptDAO = DeptDAO.getInstance();
+		List<DeptDTO> list = deptDAO.getDeptList();
+		
+		request.setAttribute("list", list);
+		request.setAttribute("title", "부서정보");
+		RequestDispatcher dispatcher = 
+			request.getRequestDispatcher("/WEB-INF/views/list.jsp");
+		dispatcher.forward(request, response);
+	}
+}
---
+++ mvc2dev_t/src/mydept/models/DeptDAO.java
@@ -0,0 +1,49 @@
+package mydept.models;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DeptDAO {
+	private static DeptDAO deptDAO = null;
+	
+	public static DeptDAO getInstance() {
+		if (deptDAO == null) {
+			deptDAO = new DeptDAO();
+		}
+		return deptDAO;
+	}
+	
+	private DeptDAO () {}
+	
+	public List<DeptDTO> getDeptList() {
+		List<DeptDTO> list = new ArrayList<>();
+		
+		StringBuffer sql = new StringBuffer();
+		sql.append(" select department_id, department_name, location_id");
+		sql.append(" from   departments");
+		sql.append(" order  by department_id asc");
+		
+		try (Connection conn = DriverManager.getConnection(
+			"jdbc:oracle:thin:@localhost:1521:xe", "n1", "n1");
+			 PreparedStatement ps = 
+				conn.prepareStatement(sql.toString());
+			 ResultSet rs = ps.executeQuery()) {
+			
+			while(rs.next()) {
+				DeptDTO deptDTO = new DeptDTO();
+				deptDTO.setDepartment_id(rs.getInt("department_id"));
+				deptDTO.setDepartment_name(rs.getString("department_name"));
+				deptDTO.setLocation_id(rs.getInt("location_id"));
+				list.add(deptDTO);
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+}
